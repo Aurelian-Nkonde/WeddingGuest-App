ea63910b5a87b5a0f4b63d4fecd4b4e22ba4d4d9
thousand10 <aurelianrobot@gmail.com>
2022-08-03 05:25:24 +0100

    setting up heroku

---
+++ Migrations/20220803042330_yha.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingInvites.Migrations
+{
+    public partial class yha : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,14 +3,31 @@ using Microsoft.EntityFrameworkCore;
 using RSVPinvites.Data;
 using RSVPinvites.Models;
 using RSVPinvites.Interfaces;
+using System.Text.RegularExpressions;
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IWeddingInvites, WeddingInvitesRepo>();
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(opt => 
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("RSVPApp")));
+// builder.Services.AddDbContext<AppDbContext>(opt => 
+//     opt.UseNpgsql(builder.Configuration.GetConnectionString("RSVPApp")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+  if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+  {
+    var m = Regex.Match(Environment.GetEnvironmentVariable("DATABASE_URL")!, @"postgres://(.*):(.*)@(.*):(.*)/(.*)");
+    options.UseNpgsql($"Server={m.Groups[3]};Port={m.Groups[4]};User Id={m.Groups[1]};Password={m.Groups[2]};Database={m.Groups[5]};sslmode=Prefer;Trust Server Certificate=true");
+  } 
+  else // In Development Environment
+  {
+    // So, use a local Connection
+    options.UseNpgsql(builder.Configuration.GetConnectionString("RSVPApp"));
+  }
+});
 
 var app = builder.Build();
 
